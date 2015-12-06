ed2f9c0d92af9e9c6d75d8b9cc6468e8dbc5844a
Jon Tejada <tejjon@gmail.com>
2015-12-05 17:14:11 -0800

    step 4 url pars with url module

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var http = require('http');
+var url = require('url');
 var server = null;
 
 var urltwo = {};
 
 function handleRequest(req, res) {
-	urltwo.pathname = req.url.split('?')[0];
-	urltwo.query = {};
-	
-	var splitQuery = req.url.split('?')[1].split('&');
-	for (var i=0; i<splitQuery.length; i++) {
-		urltwo.query[splitQuery[i].split('=')[0]] = decodeURI( splitQuery[i].split('=')[1] );
-	}
+	// urltwo.pathname = req.url.split('?')[0];
+	// urltwo.query = {};
+	// var splitQuery = req.url.split('?')[1].split('&');
+	// for (var i=0; i<splitQuery.length; i++) {
+	// 	urltwo.query[splitQuery[i].split('=')[0]] = decodeURI( splitQuery[i].split('=')[1] );
+	// }
+	// res.setHeader("Content-Type", "text/html");  
+ // 	res.end('<h1 style="font-family:sans-serif">' + JSON.stringify(urltwo.query) + '</h1>');
+ // 	console.log(urltwo);
 
-	res.setHeader("Content-Type", "text/plain");  
- 	res.end(JSON.stringify(urltwo.query));
+ 	urltwo = url.parse(req.url, true);
+ 	res.setHeader("Content-Type", "text/html"); 
+ 	res.end('<h1 style="font-family:sans-serif">' + JSON.stringify(urltwo.query) + '</h1>');
  	console.log(urltwo);
+
 }
 
 server = http.createServer(handleRequest);
